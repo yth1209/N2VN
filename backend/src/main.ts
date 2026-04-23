af461812f08bfe7b7662570f46a386421fea67ca
taehyun.yoo <yth001209@gmail.com>
2026-04-23 10:34:23 +0900

    feat: backend logging 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { LoggingInterceptor } from 'git_modules/nestjs-logger/interceptor/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS 활성화
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: false,
+  }));
+
+  const configService = app.get(ConfigService);
+  app.useGlobalInterceptors(new LoggingInterceptor(configService));
+
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
